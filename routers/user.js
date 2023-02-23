567026801548fac95df25c4820f6a5a0dcb953e0
Mia Jimenez <miajimenez@expediagroup.com>
2023-02-23 10:57:51 +0000

    add routes to user router

---
+++ routers/user.js
@@ -0,0 +1,34 @@
+const router = express.Router();
+
+// List of Users
+let users = [
+    {
+        name: "User 1",
+        age: 30
+    },
+    {
+        name: "User 2",
+        age: 45
+    },
+    {
+        name: "User 3",
+        age: 27
+    },
+    {
+        name: "User 4",
+        age: 22
+    }
+]
+
+// routes
+router.get('/', (req, res) => {
+    res.json(users);
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const findUser = users[id - 1];
+    res.json(findUser);
+})
+
+module.exports = router;
